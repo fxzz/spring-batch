7063e5bec815e523656b23edf4ba80ac3e04a3c9
fxzz <fxzzy3@gmail.com>
2023-07-08 13:22:05 +0900

    #5 - CSV 파일 데이터 읽기

--- src/main/java/com/example/springbatch/part3/ItemReaderConfiguration.java
+++ src/main/java/com/example/springbatch/part3/ItemReaderConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
@@ -36,6 +38,7 @@ public class ItemReaderConfiguration {
         return this.jobBuilderFactory.get("itemReaderJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.customItemReaderStep())
+                .next(csvFileStep())
                 .build();
     }
 
@@ -47,6 +50,53 @@ public class ItemReaderConfiguration {
                 .writer(itemWriter())
                 .build();
     }
+
+    @Bean
+    public Step csvFileStep() throws Exception {
+        return stepBuilderFactory.get("csvFileStep")
+                .<Person, Person>chunk(10)
+                .reader(csvFileItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+
+    private FlatFileItemReader<Person> csvFileItemReader() throws Exception {
+        //csv파일을 한줄씩 읽을수 있는 lineMapper
+        DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+
+        //csv파일을 <Person>객체에 매핑하기 위한 tokenizer
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        //Person객체의 필드명 매핑
+        tokenizer.setNames("id", "name", "age", "address");
+        lineMapper.setLineTokenizer(tokenizer);
+
+        //lineMapper에서 한줄씩 읽은 csv파일 내용을 객체로 변환
+        lineMapper.setFieldSetMapper(fieldSet -> {
+            int id = fieldSet.readInt("id");
+            String name = fieldSet.readString("name");
+            String age = fieldSet.readString("age");
+            String address = fieldSet.readString("address");
+
+            return new Person(id, name, age, address);
+        });
+
+        FlatFileItemReader<Person> itemReader = new FlatFileItemReaderBuilder<Person>()
+                .name("csvFileItemReader")
+                .encoding("UTF-8")
+                //new ClassPathResource 객체란 스프링에서 resources 디렉토리 안에 파일을 찾아주는 객체
+                .resource(new ClassPathResource("test.csv"))
+                //linesToSkip은 첫줄은 스킵한다. 예를들면 id,이름,나이,거주지 이런 정보가 첫줄에 있을수 있기 때문
+                .linesToSkip(1)
+                .lineMapper(lineMapper)
+                .build();
+        //아이템 리더에서 필요한 필수 설정값이 정상적으로 설정이 되었는지 검증하는 메서드
+        itemReader.afterPropertiesSet();
+
+        return itemReader;
+    }
+
+
 
     private ItemWriter<Person> itemWriter() {
         return items -> log.info(items.stream()
